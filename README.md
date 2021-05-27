32b4e33f341a8a3a840e082dd1658881aef6d322
Thomas ILLIET <contact@thomas-illiet.fr>
2021-05-28 01:10:32 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-### Hi there 👋
-
-<!--
-**thomas-illiet/thomas-illiet** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+<h2>Hey 👋, I'm <a href="https://www.linkedin.com/in/thomas-illiet">Thomas</a></h2>
+<p>I'm currently a .NET engineer based in france. I am working on some side projects, learning a couple new dishes, and trying to conquer the world of <a href="https://microsoft.com">@Microsoft</a> technologies.</p>
+<p><a href="https://www.thomas-illiet.fr"><img src="https://img.shields.io/badge/-thomas-illiet.fr-4E69C8?style=flat-square&amp;labelColor=4E69C8&amp;logo=Firefox&amp;link=https://www.thomas-illiet.fr" alt="Website Badge"></a> <a href="https://www.linkedin.com/in/thomas-illiet"><img src="https://img.shields.io/badge/-@thomas-illiet-0077B5?style=flat-square&amp;labelColor=0077B5&amp;logo=LinkedIn&amp;link=https://www.linkedin.com/in/thomas-illiet" alt="LinkedIn Badge"></a> <a href="hhttps://open.spotify.com/user/thomas-illiet"><img src="https://img.shields.io/badge/-@thomas-illiet-1ED760?style=flat-square&amp;labelColor=fff&amp;logo=Spotify&amp;link=hhttps://open.spotify.com/user/thomas-illiet" alt="Spotify Badge"></a></p>
+<img align="right" src="https://media1.giphy.com/media/13HgwGsXF0aiGY/giphy.gif" />
+<h2>⚡️ A Few Quick Facts</h2>
+<ul>
+<li>🛠   I’m currently working with .NET, PoerShell, OData, SQLServer, etc.</li>
+<li>🚀   Learning about <strong>serverless architectures</strong>, <strong>distributed systems</strong>.</li>
+<li>👨🏻‍💻   Most of my projects are available on <a href="https://github.com/thomas-illiet">Github</a>.</li>
+<li>📫   Ping me about <strong>react, koa, security, and cloud stuff</strong>.</li>
+<li>📝   Check out my <a href="https://www.linkedin.com/in/thomas-illiet">resume</a>.</li>
+<li>👾 Fun fact: Equal is Not Always Equal in Javascript.</li>
+</ul>
+<h2>⚡️ My Absolute Favorites:</h2>
+<ul>
+<li>💻   I love exploring new tech stack and building cool stuffs.</li>
+<li>📰   Reading &amp; writing tech blogs whenever possible.</li>
+<li>🍕   Hackathons, meetups &amp; tech events.</li>
+</ul>
+<h2>🚀 Some Tools I Use</h2>
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg" alt="bootstrap" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original.svg" alt=".NET" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" alt="redis" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/travis/travis-plain.svg" alt="travis" width="25" height="25" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg" alt="Docker" width="25" height="25" />
+<img src="https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg" alt="Kubernetes" width="25" height="25" />
+<img src="https://github-readme-stats.vercel.app/api?username=thomas-illiet&show_icons=true&count_private=true" alt="spiderpig86" />
+<p><img src="https://visitor-badge.glitch.me/badge?page_id=thomas-illiet.thomas-illiet" alt="visitors"></p>
---
+++ index.js
@@ -0,0 +1,149 @@
+/**
+ * README Generator
+ */
+const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    breaks: true
+});
+const mdEmoji = require('markdown-it-emoji');
+const fs = require('fs');
+const axios = require('axios').default;
+
+md.use(mdEmoji);
+
+const BLOG_HOST = `https://thomas-illiet.fr`;
+
+/* README Sections */
+const introTitle = generateTitle(2, `Hey :wave:, I'm ${generateLink('Thomas', 'https://www.linkedin.com/in/thomas-illiet')}`);
+const introDescription = `I'm currently a .NET engineer based in france. I am working on some side projects, learning a couple new dishes, and trying to conquer the world of ${generateLink('@Microsoft', 'https://microsoft.com')} technologies.`;
+
+const badgeConfigs = [{
+        name: 'Website',
+        badgeText: 'thomas-illiet.fr',
+        labelBgColor: '4E69C8',
+        logoBgColor: '4E69C8',
+        logo: 'Firefox',
+        link: 'https://www.thomas-illiet.fr',
+    },
+    {
+        name: 'LinkedIn',
+        badgeText: '@thomas-illiet',
+        labelBgColor: '0077B5',
+        logoBgColor: '0077B5',
+        logo: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/thomas-illiet',
+    },
+    {
+        name: 'Spotify',
+        badgeText: '@thomas-illiet',
+        labelBgColor: '1ED760',
+        logoBgColor: 'fff',
+        logo: 'Spotify',
+        link: 'hhttps://open.spotify.com/user/thomas-illiet',
+    },
+];
+const badges = badgeConfigs.reduce((result, config) => result + ' ' + generateBadge(config), '');
+
+const gif = `<img align="right" src="https://media1.giphy.com/media/13HgwGsXF0aiGY/giphy.gif" />`;
+const factsTitle = generateTitle(2, `:zap: A Few Quick Facts`);
+const factsConfigs = [
+    `🛠   I’m currently working with .NET, PoerShell, OData, SQLServer, etc.`,
+    `🚀   Learning about **serverless architectures**, **distributed systems**.`,
+    `👨🏻‍💻   Most of my projects are available on [Github](https://github.com/thomas-illiet).`,
+    `📫   Ping me about **react, koa, security, and cloud stuff**.`,
+    `📝   Check out my [resume](https://www.linkedin.com/in/thomas-illiet).`,
+    `👾 Fun fact: Equal is Not Always Equal in Javascript.`,
+];
+const facts = factsConfigs.reduce((result, fact) => result + `\n - ${fact}`, '');
+
+const favoritesTitle = generateTitle(2, `:zap: My Absolute Favorites:`);
+const favoritesConfigs = [
+    `💻   I love exploring new tech stack and building cool stuffs.`,
+    `📰   Reading & writing tech blogs whenever possible.`,
+    `🍕   Hackathons, meetups & tech events.`
+];
+const favorites = favoritesConfigs.reduce((result, fact) => result + `\n - ${fact}`, '');
+
+const toolsTitle = generateTitle(2, `:rocket: Some Tools I Use`)
+const toolsIconSize = 25;
+const toolsConfig = [{
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg',
+        alt: 'bootstrap',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original.svg',
+        alt: '.NET',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg',
+        alt: 'mysql',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg',
+        alt: 'redis',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg',
+        alt: 'nginx',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/travis/travis-plain.svg',
+        alt: 'travis',
+    },
+    {
+        src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg',
+        alt: 'Docker',
+    },
+    {
+        src: 'https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg',
+        alt: 'Kubernetes',
+    },
+];
+const tools = toolsConfig.reduce((result, toolConfig) => result + '\n' + generateIcon(toolConfig, toolsIconSize), '');
+
+const stats = `<img src="https://github-readme-stats.vercel.app/api?username=thomas-illiet&show_icons=true&count_private=true" alt="spiderpig86" />`;
+
+const visitors = `![visitors](https://visitor-badge.glitch.me/badge?page_id=thomas-illiet.thomas-illiet)`;
+
+(async() => {
+
+    const content = `${introTitle}\n
+${introDescription}\n
+${badges}\n
+${gif}\n
+${factsTitle}\n
+${facts}\n
+${favoritesTitle}\n
+${favorites}\n
+${toolsTitle}\n
+${tools}\n
+${stats}\n
+${visitors}
+`;
+
+    const markdownContent = md.render(content);
+
+    fs.writeFile('README.md', markdownContent, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.info(`Writing to README.md`);
+    });
+})();
+
+function generateBadge(badgeConfig) {
+    return `[![${badgeConfig.name} Badge](https://img.shields.io/badge/-${badgeConfig.badgeText}-${badgeConfig.labelBgColor}?style=flat-square&labelColor=${badgeConfig.logoBgColor}&logo=${badgeConfig.logo}&link=${badgeConfig.link})](${badgeConfig.link})`;
+}
+
+function generateIcon(iconConfig, toolsIconSize) {
+    return `<img src="${iconConfig.src}" alt="${iconConfig.alt}" width="${toolsIconSize}" height="${toolsIconSize}" />`;
+}
+
+function generateTitle(size, title) {
+    return `${'#'.repeat(size)} ${title}`;
+}
+
+function generateLink(label, link) {
+    return `[${label}](${link})`;
+}
